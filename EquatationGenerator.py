2230be6222df5f0c3d45e0cfc62778436846ae09
Nayan Patel <nayanpatel00112@gmail.com>
2023-08-04 14:00:16 +0530

    first

---
+++ EquatationGenerator.py
@@ -0,0 +1,10 @@
+# import random
+# i=0
+# while(i<500):
+#     x = str(random.random())[3:5]
+#     y = str(random.random())[3:5]
+#     print(""+str(i+1)+")" , x , "*", y , "=", (int(x)*int(y)))
+#     input()
+#     i += 1
+
+
